8ab6ef634e2c8d968e899a315cc74c0faf3476d0
Tyler Wallace <tywallace2017@gmail.com>
2020-09-08 23:04:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,9 +10,9 @@ def load_library
 end
 
 def get_japanese_emoticon
-  get_japanese_emoticon("./lib/emoticons.yml", ":)")
+  get_japanese_emoticon["./lib/emoticons.yml", ":)"]
      => "(＾ｖ＾)"
-   get_japanese_emoticon("./lib/emoticons.yml", ":o")
+   get_japanese_emoticon["./lib/emoticons.yml", ":o"]
 end
 
 def get_english_meaning
